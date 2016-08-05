2cb8f321d74e96a94d3dd081cf2ae8c6bf28a2c5
tatsuyaokubo <tatsuya.en@gmail.com>
2016-08-06 00:43:05 +0900

    modify agent

--- agent.py
+++ agent.py
@@ -1,9 +1,10 @@
 # coding:utf-8
 
+import random
 import numpy as np
+import tensorflow as tf
 from skimage.color import rgb2gray
 from skimage.transform import resize
-import tensorflow as tf
 
 from network import A3CFF
 
@@ -20,96 +21,128 @@ ACTION_INTERVAL = 4
 
 
 class Agent(object):
-    def __init__(self, thread_index, num_actions, global_network, lr_in, optimizer):
-        self.thread_index = thread_index
-        self.lr_in = lr_in
+    def __init__(self, thread_id, num_actions, global_network, lr_input, optimizer):
+        self.thread_id = thread_id
+        self.lr_input = lr_input
 
         self.local_network = A3CFF(num_actions)
         self.local_network.build_training_op()
 
-        get_gradients = tf.gradients(self.local_network.loss, self.local_network.get_vars())
-        grads_and_vars = list(zip(get_gradients, global_network.get_vars()))
-        self.grad_update = optimizer.apply_gradients(grads_and_vars)
+        get_grads = tf.gradients(self.local_network.loss, self.local_network.get_vars())
+        grads_and_vars = list(zip(get_grads, global_network.get_vars()))
+        self.grads_update = optimizer.apply_gradients(grads_and_vars)
 
-        self.sync = self.local_network.sync_from(global_network)
+        self.sync_op = self.local_network.sync_with(global_network)
 
-        self.local_t = 0
+    def get_initial_state(self, observation, last_observation):
+        processed_observation = np.maximum(observation, last_observation)
+        processed_observation = resize(rgb2gray(processed_observation), (FRAME_WIDTH, FRAME_HEIGHT))
+        state = [processed_observation for _ in range(STATE_LENGTH)]
+        return np.stack(state, axis=2)
 
-        self.repeated_action = 0
+    def get_action(self, sess, state, repeated_action, local_t):
+        action = repeated_action
 
-        self.learning_rate = INITIAL_LEARNING_RATE
-        self.lr_step = INITIAL_LEARNING_RATE / GLOBAL_T_MAX
+        if local_t % ACTION_INTERVAL == 0:
+            pi = self.local_network.get_pi(sess, state)
 
-        self.episode_reward = 0
-
-    def get_action(self, pi):
-        action = self.repeated_action
-
-        if self.local_t % ACTION_INTERVAL == 0:
             # Subtract a tiny value from probabilities in order to avoid 'ValueError: sum(pvals[:-1]) > 1.0' in np.random.multinomial
-            probs = pi - np.finfo(np.float32).epsneg
+            pi = pi - np.finfo(np.float32).epsneg
 
-            action = np.nonzero(np.random.multinomial(1, probs))[0][0]
+            action = np.nonzero(np.random.multinomial(1, pi))[0][0]
 
-            self.repeated_action = action
+            repeated_action = action
 
-        return action
+        return action, repeated_action
 
     def preprocess(self, observation, last_observation):
         processed_observation = np.maximum(observation, last_observation)
         processed_observation = resize(rgb2gray(processed_observation), (FRAME_WIDTH, FRAME_HEIGHT))
         return np.reshape(processed_observation, (FRAME_WIDTH, FRAME_HEIGHT, 1))
 
-    def process(self, sess, global_t, env, state, terminal, last_observation):
-        states = []
-        actions = []
-        rewards = []
-
-        sess.run(self.sync)
-
-        self.local_t_start = self.local_t
-
-        while not (terminal or ((self.local_t - self.local_t_start) == LOCAL_T_MAX)):
-            pi_ = self.local_network.get_policy(sess, state)
-            action = self.get_action(pi_)
-
-            observation, reward, terminal, _ = env.step(action)
-
-            states.append(state)
-            actions.append(action)
-            rewards.append(np.clip(reward, -1, 1))
-
-            processed_observation = self.preprocess(observation, last_observation)
-            next_state = np.append(state[:, :, 1:], processed_observation, axis=2)
-
-            self.local_t += 1
-
-            self.learning_rate -= self.lr_step
-            if self.learning_rate < 0.0:
-                self.learning_rate = 0.0
-
-            state = next_state
-
+    def run(self, sess, state, terminal, state_batch, action_batch, reward_batch, learning_rate, local_t, local_t_start):
         if terminal:
-            R = 0
+            r = 0
         else:
-            R = self.local_network.get_value(sess, state)
+            r = self.local_network.get_v(sess, state)
 
-        R_batch = np.zeros(self.local_t - self.local_t_start)
+        r_batch = np.zeros(local_t - local_t_start)
 
-        for i in reversed(range(self.local_t_start, self.local_t)):
-            R = rewards[i - self.local_t_start] + GAMMA * R
-            R_batch[i - self.local_t_start] = R
+        for i in reversed(range(local_t_start, local_t)):
+            r = reward_batch[i - local_t_start] + GAMMA * r
+            r_batch[i - local_t_start] = r
 
-        sess.run(self.grad_update, feed_dict={
-            self.local_network.s: states,
-            self.local_network.a: actions,
-            self.local_network.r: R_batch,
-            self.lr_in: self.learning_rate})
+        loss, _ = sess.run([self.local_network.loss, self.grads_update], feed_dict={
+            self.local_network.s: state_batch,
+            self.local_network.a: action_batch,
+            self.local_network.r: r_batch,
+            self.lr_input: learning_rate
+        })
 
-        if self.local_t % 100 == 0:
-            print('thread_id: {0} | local_t: {1}'.format(self.thread_index, self.local_t))
+        return loss
 
-        diff_local_t = self.local_t - self.local_t_start
+    def actor_learner_thread(self, env, sess, saver):
+        global global_t, learning_rate
+        global_t = 0
+        local_t = 0
+        learning_rate = INITIAL_LEARNING_RATE
+        lr_step = INITIAL_LEARNING_RATE / GLOBAL_T_MAX
+        repeated_action = 0
 
-        return diff_local_t
+        total_loss = []
+
+        terminal = False
+        observation = env.reset()
+        for _ in range(random.randint(1, NO_OP_STEPS)):
+            last_observation = observation
+            observation, _, _, _ = env.step(0)  # Do nothing
+        state = self.get_initial_state(observation, last_observation)
+
+        while global_t < GLOBAL_T_MAX:
+            local_t_start = local_t
+
+            state_batch = []
+            action_batch = []
+            reward_batch = []
+
+            sess.run(self.sync_op)
+
+            while not (terminal or ((local_t - local_t_start) == LOCAL_T_MAX)):
+                last_observation = observation
+
+                action, repeated_action = self.get_action(sess, state, repeated_action, local_t)
+
+                observation, reward, terminal, _ = env.step(action)
+
+                state_batch.append(state)
+                action_batch.append(action)
+                reward = np.clip(reward, -1, 1)
+                reward_batch.append(reward)
+
+                processed_observation = self.preprocess(observation, last_observation)
+                next_state = np.append(state[:, :, 1:], processed_observation, axis=2)
+
+                local_t += 1
+                global_t += 1
+
+                # Anneal learning rate linearly over time
+                learning_rate -= lr_step
+                if learning_rate < 0.0:
+                    learning_rate = 0.0
+
+                state = next_state
+
+            loss = self.run(sess, state, terminal, state_batch, action_batch, reward_batch, learning_rate, local_t, local_t_start)
+            total_loss.append(loss)
+
+            if terminal:
+                print('THREAD: {0:2d} / LOCAL_TIME: {1:8d} / GLOBAL_TIME: {2:10d} / LEARNING_RATE: {3:.8f} / AVG_LOSS: {4:.5f}'.format(self.thread_id, local_t, global_t, learning_rate, sum(total_loss) / len(total_loss)))
+
+                total_loss = []
+
+                terminal = False
+                observation = env.reset()
+                for _ in range(random.randint(1, NO_OP_STEPS)):
+                    last_observation = observation
+                    observation, _, _, _ = env.step(0)  # Do nothing
+                state = self.get_initial_state(observation, last_observation)
